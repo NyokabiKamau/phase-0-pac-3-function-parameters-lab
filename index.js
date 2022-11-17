ec25fdc19e1caf2d1a3cb1ddcdcedd25290ba249
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-17 23:33:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    console.log(`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function  introductionWithLanguageOptional(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+introduction("Aki");
+introductionWithLanguage("Aki", "Ember.js");
+introductionWithLanguageOptional("Gracie", "JavaScript");
+introductionWithLanguageOptional("Gracie", "Python");
