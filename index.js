f86f96708ce45104c2f62c40c9466688c055f796
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-30 12:42:25 +0300

    Resubmitted assignment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function introduction(name) {
-    console.log(`Hi, my name is ${name}.`);
+    return `Hi, my name is ${name}.`
 }
-function introductionWithLanguage(name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
-}
-function  introductionWithLanguageOptional(name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
-}
+console.log(introduction());
 
-introduction("Aki");
-introductionWithLanguage("Aki", "Ember.js");
-introductionWithLanguageOptional("Gracie", "JavaScript");
-introductionWithLanguageOptional("Gracie", "Python");
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    
+}
+console.log(introductionWithLanguage())
+
+function  introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguageOptional());
